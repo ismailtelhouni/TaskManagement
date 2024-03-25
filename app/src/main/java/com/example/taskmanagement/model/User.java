719c1a5591db5c780125324fad6c34d7fd3ed76f
ismail telhouni <ismailtelhouni123@gmail.com>
2024-03-25 02:00:00 +0000

    fragment settings finish.

--- app/src/main/java/com/example/taskmanagement/model/User.java
+++ app/src/main/java/com/example/taskmanagement/model/User.java
@@ -1,4 +1,87 @@
 package com.example.taskmanagement.model;
 
+import java.util.Objects;
+
 public class User {
+
+    private String id;
+    private String name;
+    private String lastName;
+    private String tel;
+    private String avatar;
+
+    public User(String id, String name, String lastName, String tel, String avatar) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.tel = tel;
+        this.avatar = avatar;
+    }
+
+    public User() {
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", tel='" + tel + '\'' +
+                ", avatar='" + avatar + '\'' +
+                '}';
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(lastName, user.lastName) && Objects.equals(tel, user.tel) && Objects.equals(avatar, user.avatar);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, lastName, tel, avatar);
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
 }
